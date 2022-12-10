7694d41d47298fd591b9dc4ead7ab79d9af769d9
CharosYuldashbekova <120236321+CharosYuldashbekova@users.noreply.github.com>
2022-12-11 05:55:02 +0600

    name
    public class Questions {
        public static void main(String[] args) {
            System.out.println("Name Suleyman ");
            System.out.println("Surname: Demirel");
            System.out.println("Id: 220104075");
            System.out.println("Hobby: fotball");
            System.out.println("How much do you like ICT(from 1-10)): 5");
            System.out.println("Rate this task from 1 to 10: 5");
            System.out.println("My favorite color?: 6");
        }
    }

--- Questions.java
+++ Questions.java
@@ -1,11 +1,11 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: _______");
-        System.out.println("Surname: _______");
-        System.out.println("Id: _______");
-        System.out.println("Hobby: _______");
-        System.out.println("How much do you like ICT(from 1-10)): _______");
-        System.out.println("Rate this task from 1 to 10: _______");
-        System.out.println("My favorite color?: _______");
+        System.out.println("Name Suleyman ");
+        System.out.println("Surname: Demirel");
+        System.out.println("Id: 220104075");
+        System.out.println("Hobby: fotball");
+        System.out.println("How much do you like ICT(from 1-10)): 5");
+        System.out.println("Rate this task from 1 to 10: 5");
+        System.out.println("My favorite color?: 6");
     }
 }
